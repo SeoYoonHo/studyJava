bcc8844131bf5cd0550157ceccb13943f2f80af5
seoyoonho <gogoy2643@naver.com>
2022-12-08 15:05:52 +0900

    14500 클리어

--- src/Backjoon/SamsungTest/BOJ14500/Main.java
+++ src/Backjoon/SamsungTest/BOJ14500/Main.java
@@ -1,9 +1,8 @@
 package Backjoon.SamsungTest.BOJ14500;
 
 import java.awt.*;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,6 +11,7 @@ public class Main {
             int M = sc.nextInt();
 
             int[][] map = new int[N][M];
+            int[][] visit = new int[N][M];
 
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < M; j++) {
@@ -22,25 +22,84 @@ public class Main {
             int sum = 0;
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < M; j++) {
-                    for (int type = 1; type <= 13; type++) {
-                        Point initialPoint = new Point();
-                        initialPoint.x = i;
-                        initialPoint.y = j;
-                        sum = Math.max(sumTer(map, getPointsTer(initialPoint, type)), sum);
-//                        System.out.print(sum + " ");
+                    PolygonPoint polygonPoint = new PolygonPoint();
+                    Point stPoint = new Point(i, j);
+                    polygonPoint.point = stPoint;
+                    polygonPoint.sum = map[i][j];
+                    sum = Math.max(maxSum(map, visit, polygonPoint), sum);
+
+                    for (int type = 1; type <= 4; type++) {
+                        sum = Math.max(sum,sumTer(map, getPointsTer(stPoint, type)));
                     }
                 }
-//                System.out.println();
             }
 
-//            for (int i = 0; i < N; i++) {
-//                for (int j = 0; j < M; j++) {
-//                    System.out.print(map[i][j] + " ");
-//                }
-//                System.out.println();
-//            }
-
             System.out.println(sum);
+        }
+    }
+
+    public static int maxSum(int[][] map, int[][] visit, PolygonPoint startPoint) {
+        int max = 0;
+
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+
+        Queue<PolygonPoint> queue = new LinkedList<>();
+        queue.add(startPoint);
+        visit[startPoint.point.x][startPoint.point.y] = 1;
+
+        while (!queue.isEmpty()) {
+            PolygonPoint stPolyPoint = queue.poll();
+
+            if (stPolyPoint.depth == 3) {
+                max = Math.max(max, stPolyPoint.sum);
+                continue;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                Point point = new Point();
+                point.x = stPolyPoint.point.x + dx[i];
+                point.y = stPolyPoint.point.y + dy[i];
+
+                if (point.x < 0 || point.x >= map.length || point.y < 0 || point.y >= map[0].length ||
+//                        visit[point.x][point.y] == 1 ||
+                        stPolyPoint.isContain(point)) {
+                    continue;
+                }
+
+                PolygonPoint polygonPoint = new PolygonPoint();
+                polygonPoint.point = point;
+                polygonPoint.prevPoint = stPolyPoint;
+                polygonPoint.depth = stPolyPoint.depth + 1;
+                polygonPoint.sum = polygonPoint.prevPoint.sum + map[point.x][point.y];
+                queue.add(polygonPoint);
+            }
+        }
+
+
+        return max;
+    }
+
+    static class PolygonPoint {
+        Point point;
+        PolygonPoint prevPoint;
+        int depth;
+        int sum;
+
+        boolean isContain(Point inputPoint) {
+            if (point.x == inputPoint.x && point.y == inputPoint.y) {
+                return true;
+            } else if (prevPoint == null) {
+                return false;
+            } else {
+                return prevPoint.isContain(inputPoint);
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "PolygonPoint{" + "point=" + point + ", prevPoint=" + prevPoint + ", depth=" + depth + ", sum=" +
+                    sum + '}';
         }
     }
 
@@ -55,126 +114,27 @@ public class Main {
             case 1:
                 point2.y = point.y + 1;
                 point3.y = point.y + 2;
-                point4.y = point.y + 3;
+                point4.y = point.y + 1;
                 point2.x = point.x;
                 point3.x = point.x;
-                point4.x = point.x;
+                point4.x = point.x + 1;
                 res.add(point2);
                 res.add(point3);
                 res.add(point4);
                 break;
             case 2:
-                point2.y = point.y;
-                point3.y = point.y;
-                point4.y = point.y;
-                point2.x = point.x + 1;
-                point3.x = point.x + 2;
-                point4.x = point.x + 3;
+                point2.y = point.y + 1;
+                point3.y = point.y + 1;
+                point4.y = point.y + 1;
+                point2.x = point.x - 1;
+                point3.x = point.x;
+                point4.x = point.x + 1;
                 res.add(point2);
                 res.add(point3);
                 res.add(point4);
                 break;
             case 3:
                 point2.y = point.y + 1;
-                point3.y = point.y;
-                point4.y = point.y + 1;
-                point2.x = point.x;
-                point3.x = point.x + 1;
-                point4.x = point.x + 1;
-                res.add(point2);
-                res.add(point3);
-                res.add(point4);
-                break;
-            case 4:
-                point2.y = point.y;
-                point3.y = point.y;
-                point4.y = point.y + 1;
-                point2.x = point.x + 1;
-                point3.x = point.x + 2;
-                point4.x = point.x + 2;
-                res.add(point2);
-                res.add(point3);
-                res.add(point4);
-                break;
-            case 5:
-                point2.y = point.y + 1;
-                point3.y = point.y + 2;
-                point4.y = point.y + 2;
-                point2.x = point.x;
-                point3.x = point.x;
-                point4.x = point.x - 1;
-                res.add(point2);
-                res.add(point3);
-                res.add(point4);
-                break;
-            case 6:
-                point2.y = point.y + 1;
-                point3.y = point.y + 1;
-                point4.y = point.y + 1;
-                point2.x = point.x;
-                point3.x = point.x + 1;
-                point4.x = point.x + 2;
-                res.add(point2);
-                res.add(point3);
-                res.add(point4);
-                break;
-            case 7:
-                point2.y = point.y + 1;
-                point3.y = point.y + 2;
-                point4.y = point.y;
-                point2.x = point.x;
-                point3.x = point.x;
-                point4.x = point.x + 1;
-                res.add(point2);
-                res.add(point3);
-                res.add(point4);
-                break;
-            case 8:
-                point2.y = point.y;
-                point3.y = point.y + 1;
-                point4.y = point.y + 1;
-                point2.x = point.x + 1;
-                point3.x = point.x + 1;
-                point4.x = point.x + 2;
-                res.add(point2);
-                res.add(point3);
-                res.add(point4);
-                break;
-            case 9:
-                point2.y = point.y + 1;
-                point3.y = point.y + 1;
-                point4.y = point.y + 2;
-                point2.x = point.x;
-                point3.x = point.x - 1;
-                point4.x = point.x - 1;
-                res.add(point2);
-                res.add(point3);
-                res.add(point4);
-                break;
-            case 10:
-                point2.y = point.y + 1;
-                point3.y = point.y + 2;
-                point4.y = point.y + 1;
-                point2.x = point.x;
-                point3.x = point.x;
-                point4.x = point.x + 1;
-                res.add(point2);
-                res.add(point3);
-                res.add(point4);
-                break;
-            case 11:
-                point2.y = point.y + 1;
-                point3.y = point.y + 1;
-                point4.y = point.y + 1;
-                point2.x = point.x - 1;
-                point3.x = point.x;
-                point4.x = point.x + 1;
-                res.add(point2);
-                res.add(point3);
-                res.add(point4);
-                break;
-            case 12:
-                point2.y = point.y + 1;
                 point3.y = point.y + 1;
                 point4.y = point.y + 2;
                 point2.x = point.x - 1;
@@ -184,7 +144,7 @@ public class Main {
                 res.add(point3);
                 res.add(point4);
                 break;
-            case 13:
+            case 4:
                 point2.y = point.y;
                 point3.y = point.y + 1;
                 point4.y = point.y;
