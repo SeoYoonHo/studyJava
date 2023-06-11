dbfa15e7e69f2b2311cde265e0d047ba86cf3c2e
seoyoonho <gogoy2643@naver.com>
2023-06-11 19:00:16 +0900

    item 36

--- src/EffectiveJava/EnumStudy/Main.java
+++ src/EffectiveJava/EnumStudy/Main.java
@@ -1,5 +1,7 @@
 package EffectiveJava.EnumStudy;
 
+import java.util.EnumSet;
+
 public class Main {
     public static void main(String[] args) {
         double x = 1.0;
@@ -18,5 +20,9 @@ public class Main {
         // 3.fromString 사용
         Operation plusOperation = Operation.fromString("+").orElseThrow();
         System.out.println(plusOperation.apply(x, y));
+
+        // 4. EnumSet 사용 --> 어떤 Set을 넘겨도 되나, EnumSet이 가장 좋다.
+        Text text = new Text();
+        text.applyStyle(EnumSet.of(Text.Style.BOLD, Text.Style.UNDERLINE));
     }
 }
