556f9f1432b3172410cb8d33c43a36f7a480134e
seoyoonho <gogoy2643@naver.com>
2023-06-27 14:08:34 +0900

    열거타입 함수형 인터페이스 적용(Operator)

--- src/EffectiveJava/LamdaStream/MersennePrime.java
+++ src/EffectiveJava/LamdaStream/MersennePrime.java
@@ -1,15 +1,23 @@
 package EffectiveJava.LamdaStream;
 
 import java.math.BigInteger;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.stream.Stream;
 
 public class MersennePrime {
     public static void main(String[] args) {
+        Instant beforeTime = Instant.now();
+
         primse().map(p -> BigInteger.TWO.pow(p.intValueExact())
                                         .subtract(BigInteger.ONE))
                 .filter(mersenne -> mersenne.isProbablePrime(50))
                 .limit(20)
                 .forEach(System.out::println);
+
+        Instant afterTime = Instant.now();
+        long diffTime = Duration.between(beforeTime, afterTime).toMillis(); // 두 개의 실행 시간
+        System.out.println("실행 시간(ms): " + diffTime);
     }
 
     static Stream<BigInteger> primse() {
