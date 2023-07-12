b1c6db69d68ff5f352d99cf5d4f8ef0a11ba928a
seoyoonho <gogoy2643@naver.com>
2023-07-12 15:27:25 +0900

    랜덤함수 테스트

--- src/EffectiveJava/Item1.java
+++ src/EffectiveJava/Item1.java
@@ -1,11 +1,13 @@
 package EffectiveJava;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
 import java.util.function.Supplier;
 
 public class Item1 {
     public static void main(String[] args) {
-        Supplier<Item1> supplier = () -> new Item1();
-
-        supplier.get();
+        LocalDate date = LocalDate.now();
+        int dayWeek = (int) (date.getLong(ChronoField.EPOCH_DAY) + 12) % 14 + 1;
+        System.out.println(dayWeek);
     }
 }
